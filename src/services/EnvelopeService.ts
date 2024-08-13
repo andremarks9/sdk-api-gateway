138b7c2323d25a0fe017026c91241a09b0c8c83c
andre.marques <andre.marques@ostenmoove.com.br>
2024-08-13 15:30:12 -0300

    updated

--- src/services/EnvelopeService.ts
+++ src/services/EnvelopeService.ts
@@ -1,7 +1,7 @@
 import { Inject } from '@nestjs/common'
 import axios from 'axios'
 import { AssinModule } from '../modules/AssinModule'
-import { CreateEnvelopeData, EnvelopeQuery } from '../types/envelopeTypes'
+import { CancelEnvelopeData, CloseEnvelopeData, CreateEnvelopeData, EnvelopeQuery } from '../types/envelopeTypes'
 import { TokenService } from './JWTService'
 
 export class EnvelopeService {
@@ -24,6 +24,32 @@ export class EnvelopeService {
     const userJWT = this.tokenService.get(userId)
 
     const result = await axios.post(`${AssinModule.config.apiPath}/envelope`, body, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${userJWT}`,
+      },
+    })
+
+    return result.data
+  }
+
+  async closeEnvelope(body: Array<CloseEnvelopeData>, userId?: string) {
+    const userJWT = this.tokenService.get(userId)
+
+    const result = await axios.put(`${AssinModule.config.apiPath}/envelope/close`, body, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: `Bearer ${userJWT}`,
+      },
+    })
+
+    return result.data
+  }
+
+  async cancelEnvelope(body: Array<CancelEnvelopeData>, userId?: string) {
+    const userJWT = this.tokenService.get(userId)
+
+    const result = await axios.put(`${AssinModule.config.apiPath}/envelope/cancel`, body, {
       headers: {
         Accept: 'application/json',
         Authorization: `Bearer ${userJWT}`,
